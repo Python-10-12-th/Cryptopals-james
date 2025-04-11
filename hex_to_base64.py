10508ea91ba7b44745694543db453b68274b6ef8
pul1234 <pulj250@aa.edu>
2025-04-10 23:29:34 -0600

    Improved hex to base code

--- hex_to_base64.py
+++ hex_to_base64.py
@@ -3,22 +3,22 @@ BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678
 def hex_to_bytes(hex_str):
     return bytes.fromhex(hex_str)
 
+def encode_chunk(chunk):
+    b = int.from_bytes(chunk, "big")
+    pad = 3 - len(chunk)
+    b <<= 8 * pad
+
+    chars = []
+    for j in range(4):
+        if j >= len(chunk) + 1:
+            chars.append("=")
+        else:
+            index = (b >> (18 - 6 * j)) & 0b111111
+            chars.append(BASE64_ALPHABET[index])
+    return ''.join(chars)
+
 def bytes_to_base64(byte_data):
-    base64_chars = []
-    for i in range(0, len(byte_data), 3):
-        chunk = byte_data[i:i+3]
-        b = int.from_bytes(chunk, "big")
-        pad = 3 - len(chunk)
-        b <<= 8 * pad  # pad with zeros
-
-        for j in range(4):
-            if j >= len(chunk) + 1:
-                base64_chars.append("=")
-            else:
-                index = (b >> (18 - 6 * j)) & 0b111111
-                base64_chars.append(BASE64_ALPHABET[index])
-
-    return ''.join(base64_chars)
+    return ''.join(encode_chunk(byte_data[i:i+3]) for i in range(0, len(byte_data), 3))
 
 def hex_to_base64(hex_str):
     return bytes_to_base64(hex_to_bytes(hex_str))
