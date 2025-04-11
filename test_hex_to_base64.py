99b6e5c557c2cbe8a7b788f7fdfc12548d8ea802
pul1234 <pulj250@aa.edu>
2025-04-10 23:33:54 -0600

    Note: I forgot to commit when I put the hex to base converter in

--- test_hex_to_base64.py
+++ test_hex_to_base64.py
@@ -5,7 +5,7 @@ class TestHexToBase64(unittest.TestCase):
     def test_hex_to_bytes(self):
         from hex_to_base64 import hex_to_bytes
         self.assertEqual(hex_to_bytes("4d616e"), b"Man")
-        
+            
     def test_base64_padding_1_byte(self):
         from hex_to_base64 import bytes_to_base64
         self.assertEqual(bytes_to_base64(b"M"), "TQ==")
@@ -14,7 +14,7 @@ class TestHexToBase64(unittest.TestCase):
         from hex_to_base64 import bytes_to_base64
         self.assertEqual(bytes_to_base64(b"Ma"), "TWE=")
 
-
+    
 
     def test_challenge_case(self):
         hex_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
